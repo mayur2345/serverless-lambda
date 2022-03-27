031934b694c1a9cdf02ada8823cdd77f823cc752
mayur-ltiee-shingare <mayur2345@gmail.com>
2022-03-28 02:28:44 +0530

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
     res.send('ok')
   })
 app.get('/version', function (req, res) {
+  console.log(process.env.MAYUR_VERSION)
   res.send(`Serverless ci/cd v2! ${process.env.MAYUR_VERSION}`)
 })
 
